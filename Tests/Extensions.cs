15e8f0cf0d548f85cbb58d8ebeb50d73c6ad41c3
Rafa <ingenieria5@lopez-fernandez.com>
2016-12-29 09:46:18 -0700

    RP

--- Tests/Extensions.cs
+++ Tests/Extensions.cs
@@ -7,34 +7,40 @@ using System.Threading.Tasks;
 using LibreR.Controllers;
 using LibreR.Models.Enums;
 using NUnit.Framework;
+using RestSharp;
 
 namespace Tests {
     [TestFixture]
     public static class Extensions {
-        public static void Main() {
-            Console.WriteLine(new { X = CaretPosition.End }.Serialize());
-            Thread.Sleep(999999);
-        }
-
         [Test]
-        public static void UnixEpoch() {
-            var now = DateTime.Now;
-            var timestamp = LibreR.Controllers.Extensions.DateTimeToUnixEpoch();
-            var date = LibreR.Controllers.Extensions.GetDateTimeFromUnixEpoch(timestamp);
+        public static void Foo() {
+            var client = new RestClient("http://localhost:776/keepAlive");
+            var request = new RestRequest(Method.POST);
 
-            Console.WriteLine($"Today is: {now}");
-            Console.WriteLine($"Seconds since epoch: {timestamp}");
-            Console.WriteLine($"Today should be: {date}");
+            request.AddParameter("application/json", "{\n\t\"location\": {\n\t\t\"latitude\": \"Esta!\",\n\t\t\"longitude\": \"Esta!\"\n\t}\n}", ParameterType.RequestBody);
 
-            if (
-                now.Year != date.Year &&
-                now.Month != date.Month &&
-                now.Day != date.Day &&
-                now.Hour != date.Hour &&
-                now.Minute != date.Minute &&
-                (now.Second != date.Second && now.Second != date.Second - 1)
-            )
-                Assert.Fail();
+            var response = client.Execute(request);
         }
+
+        //[Test]
+        //public static void UnixEpoch() {
+        //    var now = DateTime.Now;
+        //    var timestamp = LibreR.Controllers.Extensions.DateTimeToUnixEpoch();
+        //    var date = LibreR.Controllers.Extensions.GetDateTimeFromUnixEpoch(timestamp);
+
+        //    Console.WriteLine($"Today is: {now}");
+        //    Console.WriteLine($"Seconds since epoch: {timestamp}");
+        //    Console.WriteLine($"Today should be: {date}");
+
+        //    if (
+        //        now.Year != date.Year &&
+        //        now.Month != date.Month &&
+        //        now.Day != date.Day &&
+        //        now.Hour != date.Hour &&
+        //        now.Minute != date.Minute &&
+        //        (now.Second != date.Second && now.Second != date.Second - 1)
+        //    )
+        //        Assert.Fail();
+        //}
     }
 }
